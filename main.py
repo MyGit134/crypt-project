2f49e22c7ba05e572794f443866a56e4284c2b18
Idiot134 <leon134134@yandex.ru>
2025-04-10 20:13:42 +0300

    Завершил разработку backend-а программы.

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
 from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog
-from static.workspace import Ui_Workspace
+from data.windows.workspace import Ui_Workspace
+from data.infomodule import collectinfo
+from data.keymodule import formdeckey, formenckey
 
 
 class Workspace(QWidget, Ui_Workspace):
@@ -15,6 +17,7 @@ class Workspace(QWidget, Ui_Workspace):
         self.fileButton.clicked.connect(self.choosefile)
         self.encryptButton.clicked.connect(self.encrypt)
         self.decryptButton.clicked.connect(self.decrypt)
+        self.infoButton.clicked.connect(self.getinfo)
 
     def choosefile(self):
         self.directory = QFileDialog.getOpenFileName(self, "Выберите файл")[0]
@@ -22,6 +25,8 @@ class Workspace(QWidget, Ui_Workspace):
 
     def encrypt(self):
         self.directory = self.pathEdit.text()
+        if os.path.isdir(self.directory) and self.directory[-1] == '/':
+            self.directory = self.directory[0:-1]
         directorylist = self.directory.split('/')[0:-1]
         self.outdirectory = ''
         for i in directorylist:
@@ -45,10 +50,11 @@ class Workspace(QWidget, Ui_Workspace):
 
     def baseencrypt(self, file, filename):
         self.directory = file
-        self.key = self.passEdit.text().encode('utf-8')
-        self.key = SHA256.new(self.key).digest()
         self.nonce = get_random_bytes(12)
         self.filename = filename
+        reqdata = self.choosedata('encrypt')
+        password = SHA256.new(self.passEdit.text().encode('utf-8')).digest()
+        self.key = formenckey(password, reqdata)
         self.enc = AES.new(self.key, AES.MODE_GCM, nonce=self.nonce)
         with open(self.directory, 'rb') as filein, open(self.outdirectory + self.filename, 'wb') as fileout:
             fileout.write(self.nonce)
@@ -82,8 +88,9 @@ class Workspace(QWidget, Ui_Workspace):
                         print('Файл', str(file), "не был дешифрован. Причина: " + str(e))
 
     def basedecrypt(self, file, isdir, diroutput):
-        self.key = self.passEdit.text().encode('utf-8')
-        self.key = SHA256.new(self.key).digest()
+        password = self.passEdit.text()
+        reqdata = self.choosedata('decrypt')
+        self.key = formdeckey(password, reqdata)
         self.directory = file
         with open(self.directory, 'rb') as filein:
             self.nonce = filein.read(12)
@@ -124,6 +131,48 @@ class Workspace(QWidget, Ui_Workspace):
                 if os.path.exists(temp_path):
                     os.remove(temp_path)
                 print(f'Ошибка при дешифровке файла {self.filename}: {e}')
+
+    def getinfo(self):
+        data = collectinfo()
+        self.macinfoEdit.setText(data[0])
+        self.cpuinfoEdit.setText(data[1])
+        self.uuidinfoEdit.setText(data[2])
+        self.serialinfoEdit.setText(data[3])
+        self.gpuserialinfoEdit.setText(data[4])
+        self.mbserialinfoEdit.setText(data[5])
+
+    def choosedata(self, datafor):
+        mac, cpu_name, system_uuid, disk_serial, gpu_serial, mb_serial = b'', b'', b'', b'', b'', b''
+        decdata = []
+        if self.macBox.isChecked():
+            decdata.append('mac')
+            if self.macEdit.text():
+                mac = SHA256.new(self.macEdit.text().encode('utf-8')).digest()
+        if self.cpuBox.isChecked():
+            decdata.append('cpu_name')
+            if self.cpuEdit.text():
+                cpu_name = SHA256.new(self.cpuEdit.text().encode('utf-8')).digest()
+        if self.uuidBox.isChecked():
+            decdata.append('system_uuid')
+            if self.uuidEdit.text():
+                system_uuid = SHA256.new(self.uuidEdit.text().encode('utf-8')).digest()
+        if self.diskserialBox.isChecked():
+            decdata.append('disk_serial')
+            if self.diskserialEdit.text():
+                disk_serial = SHA256.new(self.diskserialEdit.text().encode('utf-8')).digest()
+        if self.gpuserialBox.isChecked():
+            decdata.append('gpu_serial')
+            if self.gpuserialEdit.text():
+                gpu_serial = SHA256.new(self.gpuserialEdit.text().encode('utf-8')).digest()
+        if self.mbserialBox.isChecked():
+            decdata.append('mb_serial')
+            if self.mbserialEdit.text():
+                mb_serial = SHA256.new(self.mbserialEdit.text().encode('utf-8')).digest()
+        encdata = [mac, cpu_name, disk_serial, mb_serial, gpu_serial, system_uuid]
+        if datafor == 'encrypt':
+            return encdata
+        elif datafor == 'decrypt':
+            return decdata
 
 
 def main():
